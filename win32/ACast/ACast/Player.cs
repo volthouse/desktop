b48531fa3cc6faaf5faedd15b2195bb58022852a
volthouse <lucasgaertner@arcor.de>
2016-01-07 21:20:02 +0100

    Flyout testing.

--- win32/ACast/ACast/Player.cs
+++ win32/ACast/ACast/Player.cs
@@ -1,5 +1,4 @@
-﻿using ACastBackgroundAudioTask;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
--- win32/ACast/ACast/PlayerControl.xaml.cs
+++ win32/ACast/ACast/PlayerControl.xaml.cs
@@ -23,5 +23,8 @@ namespace ACast
         {
             this.InitializeComponent();
         }
+
+        
+        
     }
 }
